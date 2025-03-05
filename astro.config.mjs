bc07d8cc743041e087a1198438f07b935266d232
Jesus-007-cmd <jesusgtz1@hotmail.com>
2025-03-05 00:50:16 -0600

    Se resolvieron conflictos de merge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
-// @ts-check
+﻿// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     
     publicDir: 'public', // Asegura que está permitiendo acceder a public/
+    vite: {
+      plugins: [],
+      server: {
+        headers: {
+          "Content-Type": "text/html; charset=UTF-8",
+        },
+      },
+    },
   });
+
+  
