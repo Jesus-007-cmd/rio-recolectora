7e2b6c7b754c46cfa62d13a32d5ecc915f689979
Jesus-007-cmd <jesusgtz1@hotmail.com>
2025-03-11 23:44:09 -0600

    se realizan ajustes de colores e imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 ﻿// @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
     
     publicDir: 'public', // Asegura que está permitiendo acceder a public/
     vite: {
-      plugins: [],
+      plugins: [tailwindcss()],
       server: {
         headers: {
           "Content-Type": "text/html; charset=UTF-8",
@@ -13,5 +15,3 @@ export default defineConfig({
       },
     },
   });
-
-  
